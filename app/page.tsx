744ba2987a84fe057afaa02b6ba9474e7855508a
Lusfan <nevzlom.vtk@mail.ru>
2024-07-25 02:21:28 +0300

    header complited, 50:32

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <>
-      <h1>Привет</h1>
-      <Button variant="outline">Привет</Button>
+      Главная
     </>
   );
 }
