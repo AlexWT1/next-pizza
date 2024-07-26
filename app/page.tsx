03060b163e1c59f3fe8797d41169bc7a29db4e83
Lusfan <nevzlom.vtk@mail.ru>
2024-07-27 02:08:07 +0300

    title, top-bar, 1:03:37

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Categories, Container, SortPopup, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
    <>
-      Главная
+      <Container className="mt-8">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      
+      <TopBar />
     </>
   );
 }
