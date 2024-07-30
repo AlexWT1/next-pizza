5a81097e7f5d79746534112c7c3b0f97fa67fc6f
Alex <99095805+AlexWT1@users.noreply.github.com>
2024-07-31 00:54:35 +0300

    sidebar, 1:17:06

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
-import { Categories, Container, SortPopup, Title, TopBar } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
-   <>
+    <>
       <Container className="mt-8">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
-      
       <TopBar />
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/* Фильтарция */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">СПИСОК ТОВАРОВ</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,2 +1,8 @@
 export { Header } from './header';
 export { Container } from './container';
+export { SortPopup } from './sort-popup';
+export { Categories } from './categories';
+export { Title } from './title';
+export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
