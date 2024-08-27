d2150f70f6718f188862d086f00f7dc0b0b02039
Alex <99095805+AlexWT1@users.noreply.github.com>
2024-08-28 01:27:03 +0300

    2:24:34

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           {/* Фильтарция */}
           <div className="w-[250px]">
             <Filters />
@@ -34,8 +34,102 @@ export default function Home() {
                     price: 550,
                     items: [{ price: 550 }],
                   },
+                  {
+                    id: 2,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
                 ]}
                 categoryId={1}
+              />
+              <ProductsGroupList
+                title="Комбо"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: 'Чизбургер-пицаа',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:292x292/11EE7D6134BC4150BDD8E792D866AB52.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={2}
               />
             </div>
           </div>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,4 +1,6 @@
+'use client';
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 import Link from 'next/link';
 import React from 'react';
 
@@ -6,23 +8,48 @@ interface Props {
   className?: string;
 }
 
-const cats = ['Пиццы', 'Комбо', 'Закуски', 'Коктели', 'Кофе', 'Напитки', 'Десерты'];
-const activeIndex = 0;
+const cats = [
+  {
+    id: 1,
+    name: 'Пиццы',
+  },
+  {
+    id: 2,
+    name: 'Комбо',
+  },
+  {
+    id: 3,
+    name: 'Закуски',
+  },
+  {
+    id: 4,
+    name: 'Коктели',
+  },
+  {
+    id: 5,
+    name: 'Кофе',
+  },
+  {
+    id: 6,
+    name: 'Десерты',
+  },
+];
 
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoyAcriveId = useCategoryStore((state) => state.activeId);
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-      {cats.map((name, i) => (
-        <Link
-          key={name}
+      {cats.map(({ name, id }, i) => (
+        <a
+          key={i}
           className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5',
-            activeIndex === i && 'bg-white shadow-md shadow-gray-200 text-primary',
+            categoyAcriveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
           )}
           href=""
         >
-          {name}
-        </Link>
+          <button>{name}</button>
+        </a>
       ))}
     </div>
   );
