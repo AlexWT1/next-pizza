a0ac2e5c5f90aacbe0040a748f559d3d973d5a39
Lusfan <nevzlom.vtk@mail.ru>
2024-09-08 16:20:06 +0300

    5:53:49

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import { Button } from '../ui';
 import { Container } from './container';
 import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import Link from 'next/link';
+import { SearchInput } from './search-input';
 
 interface Props {
     className?: string;
@@ -16,13 +18,19 @@ export const Header: React.FC<Props> = ({ className }) => {
 
 
             {/* Левая часть */}
+           <Link href="/">
             <div className='flex items-center gap-4'>
-                <Image src="/logo.png" alt="Logo" width={35} height={35} />
-                <div>
-                    <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
-                    <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                    <Image src="/logo.png" alt="Logo" width={35} height={35} />
+                    <div>
+                        <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
+                        <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                    </div>
                 </div>
-            </div>
+           </Link>
+
+           <div className="mx-10 flex-1">
+                <SearchInput />
+           </div>
              {/* Правая часть */}
             <div className="flex items-center gap-3">
                 <Button variant="outline" className='felx items-center gap-1'>
